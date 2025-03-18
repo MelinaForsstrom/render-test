b35d7a3bea32b1583e1b81b13bf97f1c3d239c2a
Melina Forsstroem <melina.forsstrom@hotmail.com>
2025-03-18 21:03:47 +0100

    Initial commit for Render deployment

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+
+
+let notes = [
+    {
+      id: "1",
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: "2",
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: "3",
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+  //Middleware (before paths)
+
+  const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
+  app.use(express.json())
+  app.use(requestLogger)
+
+  //CORS Middleware (before paths)
+
+  app.use(cors())
+
+  //GET REQUESTS
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  //POST REQUESTS
+
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
+ 
+
+  //DELETE REQUESTS
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+  
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+
+
+
+  //Middleware (after paths)
+
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
